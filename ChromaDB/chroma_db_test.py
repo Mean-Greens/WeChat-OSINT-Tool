d18a9873fb4e5864b32ad026771046f805c70201
D5qu1r7 <112665636+davisholdstock@users.noreply.github.com>
2024-11-08 09:11:16 -0700

    Update (chroma_db_test.py) to include vector embeddings

--- ChromaDB/chroma_db_test.py
+++ ChromaDB/chroma_db_test.py
@@ -1,37 +1,52 @@
+import ollama
 import chromadb
 
-client = chromadb.PersistentClient(path='./db')
+'''
+pip install ollama chromadb
+'''
+
+client = chromadb.PersistentClient(path='c:/Users/capstone/Desktop/db')
 
 collection = client.get_or_create_collection(name="test")
 
 if collection.count() == 0:
-    collection.add(
-        documents=[
-            "This is a document about machine learning",
-            "This is another document about data science",
-            "A third document about artificial intelligence"
-        ],
-        metadatas=[
-            {"source": "test1"},
-            {"source": "test2"},
-            {"source": "test3"}
-        ],
-        ids=[
-            "id1",
-            "id2",
-            "id3"
-        ]
-    )
 
-print(collection.count())
-print(client.list_collections())
-print()
+    documents = [
+    "Llamas are members of the camelid family meaning they're pretty closely related to vicuñas and camels",
+    "Llamas were first domesticated and used as pack animals 4,000 to 5,000 years ago in the Peruvian highlands",
+    "Llamas can grow as much as 6 feet tall though the average llama between 5 feet 6 inches and 5 feet 9 inches tall",
+    "Llamas weigh between 280 and 450 pounds and can carry 25 to 30 percent of their body weight",
+    "Llamas are vegetarians and have very efficient digestive systems",
+    "Llamas live to be about 20 years old, though some only live for 15 years and others live to be 30 years old",
+    ]
+    
+    # store each document in a vector embedding database
+    for i, d in enumerate(documents):
+        response = ollama.embeddings(model="mxbai-embed-large", prompt=d)
+        embedding = response["embedding"]
+        collection.add(
+            ids=[str(i)],
+            embeddings=[embedding],
+            metadatas=[{"source": f'test{i}'}],
+            documents=[d]
+        )
 
-results = collection.query(
-    query_texts=[
-        "This is a query about machine learning and data science"
-    ],
-    n_results=2
+#------------------------------------------------------ Find how many articles are in the database ------------------------------------------------------#
+# print(collection.count())
+# print()
+
+# an example prompt
+prompt = "What animals are llamas related to?"
+
+# generate an embedding for the prompt and retrieve the most relevant doc
+response = ollama.embeddings(
+  model="mxbai-embed-large",
+  prompt=prompt
 )
+results = collection.query(
+  query_embeddings=[response["embedding"]],
+  n_results=1
+)
+data = results['documents'][0][0]
 
-print(results)
+print(data)
