a3b43e8019180605a53ad3c4877b67bfc2274f44
Davis Holdstock <daviswholdstock@gmail.com>
2024-12-11 09:12:22 -0700

    Update (check_for_working_proxies.py) scrape proxies from open source sites

--- proxies/check_for_working_proxies.py
+++ proxies/check_for_working_proxies.py
@@ -1,27 +1,60 @@
-import asyncio
+import concurrent
+import time
 import httpx
+import re
+from bs4 import BeautifulSoup
 
-proxies = open('proxy_list.txt').read().splitlines()
+def gather_proxy_list():
+    regex = r"[0-9]+(?:\.[0-9]+){3}:[0-9]+"
+    c = httpx.get("https://spys.me/proxy.txt")
+    test_str = c.text
+    a = re.finditer(regex, test_str, re.MULTILINE)
+    with open("proxies_list.txt", 'w') as file:
+        for i in a:
+            print(i.group(),file=file)
+            
+    d = httpx.get("https://free-proxy-list.net/")
+    soup = BeautifulSoup(d.content, 'html.parser')
+    td_elements = soup.select('.fpl-list .table tbody tr td')
+    ips = []
+    ports = []
+    for j in range(0, len(td_elements), 8):
+        ips.append(td_elements[j].text.strip())
+        ports.append(td_elements[j + 1].text.strip())
+    with open("proxies_list.txt", "a") as myfile:
+        for ip, port in zip(ips, ports):
+            proxy = f"{ip}:{port}"
+            print(proxy, file=myfile)
 
-async def check_proxy(proxy):
+def check_proxy(proxy, file):
 
-    proxies_1 = {
-        "http://": f"http://{proxy}",
-        "https://": f"http://{proxy}",
+    proxy_url = f"http://{proxy}"
+
+    # set up proxy mounts
+    proxy_mounts = {
+        "http://": httpx.HTTPTransport(proxy=proxy_url),
+        "https://": httpx.HTTPTransport(proxy=proxy_url),
     }
 
-    # make a request using the selected proxy
+    # Try a request with the proxy
     try:
-        async with httpx.AsyncClient(proxies=proxies_1, timeout=5.0, verify=False) as client:
-            response = await client.get("https://httpbin.org/ip")
-            response.raise_for_status()
-            print(f"Proxy {proxy} is working.")
-    except (httpx.RequestError, httpx.HTTPStatusError) as exc:
+        with httpx.Client(mounts=proxy_mounts) as client:
+            response = client.get("https://httpbin.org/ip")
+            print(proxy, file=file)
+    except Exception as e:
         return
-        print(f"Proxy {proxy} failed: {exc}")
 
-async def main():
-    tasks = [check_proxy(proxy) for proxy in proxies] 
-    await asyncio.gather(*tasks)
+def main():
+    gather_proxy_list()
 
-asyncio.run(main())
+    time.sleep(1)
+
+    proxies = open('proxies_list.txt').read().splitlines()
+
+    with open("working_proxies_list.txt", 'w') as file:
+        with concurrent.futures.ThreadPoolExecutor(max_workers=6) as executor:
+            futures = [executor.submit(check_proxy, proxy, file) for proxy in proxies[:100]] 
+            concurrent.futures.wait(futures)
+
+if __name__ == "__main__":
+    main()
