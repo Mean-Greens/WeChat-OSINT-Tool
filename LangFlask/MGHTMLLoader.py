8670c3b81d0be22e8c0bc4cd274d2da8001aeed3
Davis Holdstock <daviswholdstock@gmail.com>
2025-02-18 12:34:23 -0700

    Update (MGHTMLLoader) walk folder structure looking for html files

--- LangFlask/MGHTMLLoader.py
+++ LangFlask/MGHTMLLoader.py
@@ -168,76 +168,78 @@ class MGHTMLLoader(BaseLoader):
 def reload_database(folder_path):
     documents = []
 
-    for file_name in os.listdir(folder_path):
-        if file_name.endswith(".html"):
-            file_path = os.path.join(folder_path, file_name)
-            soup = BeautifulSoup(open(file_path, "r", encoding="utf-8"), "html.parser")
+    # Walk through the directory structure
+    for root, dirs, files in os.walk(folder_path):
+        for file_name in files:
+            if file_name.endswith(".html"):
+                file_path = os.path.join(folder_path, file_name)
+                soup = BeautifulSoup(open(file_path, "r", encoding="utf-8"), "html.parser")
+                
+                # Get a string of the html to save to folder of html files
+                website = str(soup)
+
+                # Strip the text out of the html and normalize it
+                body = soup.body
+                body_text = body.get_text(strip=True)
+                normalized_text = unicodedata.normalize("NFKD", body_text)
+                normalized_text = ' '.join(normalized_text.split())
+
+                # Get the metadata
+                metadata_folder = os.path.join(os.path.dirname(__file__), "metadata")
+                metadata_file_path = os.path.join(metadata_folder, "metadata_deduplicated.json")
+
+                with open(metadata_file_path, "r", encoding="utf-8") as metadata_file:
+                    metadata = json.load(metadata_file)
+
+                for data in metadata:
+                    if data["key"] == "hash" and data["string_value"] == file_name.replace(".html", ""):
+                        id = data["id"]
+                        break
+
+                data_matching_hash = []
+                for data in metadata:
+                    if data["id"] == id:
+                        data_matching_hash.append(data)
+
+                source = ""
+                title = ""
+                description = ""
+                author = ""
+                date = ""
+                keyword = ""
+
+                for data in data_matching_hash:
+                    if data["key"] == "source":
+                        source = data["string_value"]
+                    elif data["key"] == "title":
+                        title = data["string_value"]
+                    elif data["key"] == "description":
+                        description = data["string_value"]
+                    elif data["key"] == "author":
+                        author = data["string_value"]
+                    elif data["key"] == "date":
+                        date = data["string_value"]
+                    elif data["key"] == "keyword":
+                        keyword = data["string_value"]
+
+                # rehash the article
+                web_hash = hashlib.sha256(normalized_text.encode('utf-8')).hexdigest()
+
+                metadata: Dict[str, Union[str, None]] = {
+                "source": source,
+                "title": title,
+                "description": description,
+                "author": author,
+                "date": date,
+                "hash": web_hash,
+                "keyword": keyword
+                }
+
+                # Create document objects for the website and the normalized text to store in the database
+                doc = [Document(page_content=website, metadata=metadata), Document(page_content=normalized_text, metadata=metadata)]
             
-            # Get a string of the html to save to folder of html files
-            website = str(soup)
-
-            # Strip the text out of the html and normalize it
-            body = soup.body
-            body_text = body.get_text(strip=True)
-            normalized_text = unicodedata.normalize("NFKD", body_text)
-            normalized_text = ' '.join(normalized_text.split())
-
-            # Get the metadata
-            metadata_folder = os.path.join(os.path.dirname(__file__), "metadata")
-            metadata_file_path = os.path.join(metadata_folder, "metadata_deduplicated.json")
-
-            with open(metadata_file_path, "r", encoding="utf-8") as metadata_file:
-                metadata = json.load(metadata_file)
-
-            for data in metadata:
-                if data["key"] == "hash" and data["string_value"] == file_name.replace(".html", ""):
-                    id = data["id"]
-                    break
-
-            data_matching_hash = []
-            for data in metadata:
-                if data["id"] == id:
-                    data_matching_hash.append(data)
-
-            source = ""
-            title = ""
-            description = ""
-            author = ""
-            date = ""
-            keyword = ""
-
-            for data in data_matching_hash:
-                if data["key"] == "source":
-                    source = data["string_value"]
-                elif data["key"] == "title":
-                    title = data["string_value"]
-                elif data["key"] == "description":
-                    description = data["string_value"]
-                elif data["key"] == "author":
-                    author = data["string_value"]
-                elif data["key"] == "date":
-                    date = data["string_value"]
-                elif data["key"] == "keyword":
-                    keyword = data["string_value"]
-
-            # rehash the article
-            web_hash = hashlib.sha256(normalized_text.encode('utf-8')).hexdigest()
-
-            metadata: Dict[str, Union[str, None]] = {
-            "source": source,
-            "title": title,
-            "description": description,
-            "author": author,
-            "date": date,
-            "hash": web_hash,
-            "keyword": keyword
-            }
-
-            # Create document objects for the website and the normalized text to store in the database
-            doc = [Document(page_content=website, metadata=metadata), Document(page_content=normalized_text, metadata=metadata)]
-        
-            # The store_websites function is a function that stores the websites in the database and takes a list of document objects
-            documents.append(doc)
+                # The store_websites function is a function that stores the websites in the database and takes a list of document objects
+                documents.append(doc)
 
     # store the websites in the database
     store_websites(documents)
