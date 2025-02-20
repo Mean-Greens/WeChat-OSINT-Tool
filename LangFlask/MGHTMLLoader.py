a35e3bf66d8412621ba6a92b2a853917d316bd39
Mr Green <Mrgreen@byu.edu>
2025-02-19 20:00:59 -0700

    Update (MGHTMLLoader.py) fix file path issue when walking the folder structure

--- LangFlask/MGHTMLLoader.py
+++ LangFlask/MGHTMLLoader.py
@@ -172,7 +172,7 @@ def reload_database(folder_path):
     for root, dirs, files in os.walk(folder_path):
         for file_name in files:
             if file_name.endswith(".html"):
-                file_path = os.path.join(folder_path, file_name)
+                file_path = os.path.join(root, file_name)
                 soup = BeautifulSoup(open(file_path, "r", encoding="utf-8"), "html.parser")
                 
                 # Get a string of the html to save to folder of html files
