0c341a40021fd2a16aefa7526a37f4a89e88e701
Mr Green <Mrgreen@byu.edu>
2025-02-27 08:54:27 -0700

    cleanup

--- LangFlask/query.py
+++ LangFlask/query.py
@@ -2,6 +2,7 @@
 
 import os
 import sys
+import datetime
 from langchain_ollama import ChatOllama
 from langchain.prompts import ChatPromptTemplate, PromptTemplate
 from langchain_core.output_parsers import StrOutputParser
@@ -14,6 +15,7 @@ sys.path.append(os.path.dirname(os.path.abspath(__file__)))
 from get_vector_db import get_vector_db, get_chunked_db
 
 LLM_MODEL = os.getenv('LLM_MODEL', 'aya-expanse:32b')
+#LLM_MODEL = 'deepseek-r1:32b'
 
 # Function to get the prompt templates for generating alternative questions and answering based on context
 # This gives the LLM a kind of base behavior to work off of. 
@@ -31,8 +33,9 @@ def get_prompt():
 
     template = """Ignore all previous instructions. You are an AI assistant that specializes in OSINT. You must respond ONLY in English. 
 
-    You MUST answer the question **ONLY** using the information from the provided context. **Do not use outside knowledge.** If the context does not contain relevant information to answer the question, follow these steps:
-
+    You MUST answer the question **ONLY** using the information from the provided context. **Do not use outside knowledge.** Provide any information from the context that would be meaningful to an OSINT professional.
+    
+    If the context does not contain relevant information to answer the question, follow steps 1 and 2:
     1. Clearly state: *"I cannot find any useful information in the provided documents."*
     2. Provide **exactly five** search terms the user could use in a search engine to find relevant answers.
 
@@ -42,9 +45,9 @@ def get_prompt():
     **Question:** {question}
 
     **Important Rules:**
-    - If the answer is in the context, provide a direct answer using ONLY that information.
+    - Answer **ONLY** in English
+    - If the answer is in the context, provide an answer using ONLY that information.
     - If the answer is NOT in the context, DO NOT GUESS. DO NOT provide external knowledge.
-    - If needed, list **exactly five** search terms for further research.
     """
 
     #Answer the question based ONLY on the following context:
@@ -72,29 +75,55 @@ def query(input):
         # )
         
         output = db.as_retriever(search_kwargs={'k': 5})
+        output_full = db.as_retriever(search_kwargs={'k': 15})
         
         translated_input = query_translation(input)
         print(translated_input)
-        context_docs = output.invoke(translated_input)
         
-        for doc in context_docs:
-            print(doc.metadata.get('hash'))
-            print(doc.metadata.get('title'))
-            print(doc.page_content)
-
         # Define the processing chain to retrieve context, generate the answer, and parse the output
         chain = (
-            {"context": context_docs, "question": RunnablePassthrough()}
+            {"context": output | (lambda docs: print_docs(docs)), "question": RunnablePassthrough()}
+            #{"context": output, "question": RunnablePassthrough()}
             | prompt
             | llm
             | StrOutputParser()
         )
 
-        response = chain.invoke(input)
+
+        global sources
+        sources = "\n\nSources:\n\n"
+
+        def print_docs(docs):
+            global sources
+            for doc in docs:
+                sources += ("Article Title: " + doc.metadata.get('title') + "\n")
+                sources += ("   Author: " + doc.metadata.get('author') + "\n")
+                sources += ("   Date: " + timeConvert(doc.metadata.get('date')) + "\n")
+                sources += ("   Hash: " + doc.metadata.get('hash') + "\n\n")
+            return docs
+
+        # context_docs = output.invoke(input)
+        output_full_list = output_full.invoke(translated_input)
         
-        return response
+        # for doc in context_docs:
+        #     sources += ("Article Title: " + doc.metadata.get('title') + "\n")
+        #     sources += ("   Author: " + doc.metadata.get('author') + "\n")
+        #     sources += ("   Date: " + timeConvert(doc.metadata.get('date')) + "\n")
+        #     sources += ("   Hash: " + doc.metadata.get('hash') + "\n\n")
+
+        response = chain.invoke(translated_input)
+
+        
+        return response + sources
 
     return None
+
+
+def timeConvert(unix_timestamp): 
+    # Convert the Unix timestamp to a datetime object 
+    dt = datetime.datetime.fromtimestamp(int(unix_timestamp)) 
+    # Format the datetime object as a string 
+    return dt.strftime('%Y-%m-%d %H:%M:%S')
 
 # Main function to handle the query translation process. 
 def query_translation(input):
@@ -103,7 +132,7 @@ def query_translation(input):
         # Initialize the language model with the specified model name
         llm = ChatOllama(model=LLM_MODEL)
 
-        response = llm.invoke(f"translate the following word or phrase for me. I would like it translated to chinese (traditional). The word or phrase is '{input}'. please only return the translation and nothing else")
+        response = llm.invoke(f"Translate the following word or phrase for me. I would like it translated to simplified chinese. The word or phrase is '{input}'. Please only return the translation and nothing else")
         
         return response.content
 
